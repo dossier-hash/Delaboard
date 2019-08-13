86d18282c7e5cdda726e54723853a83c43fc2355
dossier-hash <dayy218@gmail.com>
2019-08-13 15:46:07 +0500

    Formatting and misc changes

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy 
 
+
+
 app = Flask(__name__)
 dbf = "sqlite:///dela.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = dbf
@@ -12,6 +14,7 @@ db = SQLAlchemy(app)
 class Post(db.Model):
 	id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
 	board = db.Column(db.String(2), nullable=False)
+	#image = db.Column(db.Photo(root='/photos'))
 	content = db.Column(db.String(200000))
 
 	def __str__(self):
@@ -45,7 +48,7 @@ def board(board):
 		new_post = Post(content=post_content, board=post_board)
 		db.session.add(new_post)
 		db.session.commit()
-		return redirect('/g/')
+		return redirect('/'+ board)
 	else:
 		if board in ok_boards.keys():
 			posts = Post.query.filter_by(board=board).all()
